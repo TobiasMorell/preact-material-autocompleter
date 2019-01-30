ad382b6fdaa5699f2eabfd103bf79fce077c1ca4
Tobias Morell <tobiasmorell@hotmail.com>
2019-01-30 16:48:04 +0100

    Add source files

---
+++ index.js
@@ -0,0 +1,136 @@
+import { h, Component } from 'preact';
+import style from './style.css';
+import TextField from 'preact-material-components/TextField';
+import 'preact-material-components/TextField/style.css';
+
+/**
+ * Material design autocompleter. Opens up a dropdown when the TextField is selected, where the user may select one of
+ * the items in a collection.
+ */
+export default class AutoCompleter extends Component {
+	state = {
+		autocompleting: false,
+		currentInput: '',
+		relevantItems: []
+	};
+
+	/**
+	 * Gets the current value of the autocompleter.
+	 * @returns {string}
+	 */
+	getRecommendation = () => this.state.currentInput;
+
+	/**
+	 * Sets the current value of the autocompleter.
+	 * @param value {string} The value to set.
+	 */
+	setRecommendation = (value) => {
+		this.setState({ currentInput: value });
+	};
+
+	/**
+	 * Autocomplete by reading the value of an option and setting the TextField's value to that.
+	 * @param e {event} onClick event object.
+	 * @private
+	 */
+	_autocomplete = (e) => {
+		let input;
+		if (e.target.children.length)
+			input = e.target.children[0].innerHTML;
+		else
+			input = e.target.innerHTML;
+		this.setState({ currentInput: input });
+		this._stop();
+	};
+
+	/**
+	 * Create an item for the dropdown menu.
+	 * @param itemName {string} The name of the item to create.
+	 * @returns {*} An HTML element representing the item.
+	 * @private
+	 */
+	_createItem(itemName) {
+		return (
+			<div onClick={this._autocomplete}><strong>{itemName}</strong></div>
+		);
+	}
+
+	/**
+	 * Starts the autocompleter by showing the dropdown menu and resetting the value of the input field.
+	 */
+	start = () => {
+		this.setState({ autocompleting: true });
+		this._onInput({ target: { value: '' } });
+	};
+	/**
+	 * Stops the auto completion by hiding the dropdown menu and firing the onChange event, if that is set.
+	 * @private
+	 */
+	_stop = () => {
+		this.setState({ autocompleting: false });
+		if (this.state.onChange)
+			this.state.onChange({
+				target: { value: this.getRecommendation() }
+			});
+	};
+	/**
+	 * Abort the autocompletion by hiding the dropdown. The onChange event is not fired when aborting autocompletion.
+	 */
+	abort = () => {
+		setTimeout(() => {
+			this.setState({ autocompleting: false });
+		}, 100);
+	};
+
+	/**
+	 * Responds to a user keystroke by finding suggestions and updating the dropdown correspondingly.
+	 * @param e {event} - The onInput event object from the TextField.
+	 * @private
+	 */
+	_onInput = (e) => {
+		let newVal = e.target.value;
+
+		let matches;
+		if (this.state.allItems) {
+			matches = newVal ? this.state.allItems.filter(item => item.includes(newVal)) : this.state.allItems;
+		}
+		else
+			matches = [];
+		this.setState({
+			relevantItems: matches,
+			currentInput: newVal
+		});
+	};
+
+	addNewCategory = e => {
+		this._stop();
+		this.setState({allItems: allItems.concat(this.state.currentInput)});
+	};
+
+	render({ items, classNames, allowAddNewItems = false, hintText = undefined, id = undefined, onChange = undefined }, state, context) {
+		this.state.allItems = items;
+		this.state.onChange = onChange;
+
+		let suggestions;
+		if (this.state.relevantItems.length > 0)
+			suggestions = this.state.relevantItems.map(i => this._createItem(i));
+		else if (allowAddNewItems)
+			suggestions = <div onClick={this.addNewCategory}><strong>No such category, click here to add it.</strong></div>;
+		else
+			suggestions = <div><strong>No item matches your search.</strong></div>;
+
+		return (
+			<div className={classNames}>
+				<div className={style.autocomplete}>
+					<TextField className={style.wideInputField} id={id} label={hintText}
+							   onInput={this._onInput}
+							   onfocusin={this.start} value={this.state.currentInput} onfocusout={e => this.abort()}
+					/>
+					<div className={[style['autocomplete-items'], this.state.autocompleting ? style.active : ''].join(' ')} >
+						{suggestions}
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
